739eac2879b621fd291fdd4f74d590c0ebabc4ff
Camilla <camilla.lima000@gmail.com>
2021-11-08 13:52:26 +0000

    added test for top up feature

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,8 @@ describe Oystercard do
   it "has a balance of zero" do
     expect(subject.balance).to eq(0)
   end
+
+  it "can top up the balance" do
+    expect{ subject.top_up(10) }.to change{ subject.balance }.by(10)
+  end
 end
